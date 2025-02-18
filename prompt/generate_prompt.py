b50046eb975e14311821f3af803290069cf7c320
ivankching <ivan1335@gmail.com>
2025-02-18 10:20:18 +0800

    Add methods to create prompt for cmd line input

--- prompt/generate_prompt.py
+++ prompt/generate_prompt.py
@@ -5,8 +5,16 @@ from pathlib import Path
 
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
+from langchain.prompts import ChatPromptTemplate
 
+load_dotenv(find_dotenv())
 ROOT = Path(__file__).parent.parent
+PROMPT_TEMPLATE = """
+Answer the question using only the following context:
+{context}
+-------------------------------------------------------------
+Answer this question based on the context above: {question} 
+"""
 
 def query_db(query):
     embedding_fn =  HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-l6-v2")
@@ -18,3 +26,20 @@ def query_db(query):
         return
     
     return results
+
+def create_prompt(query):
+    context = query_db(query)
+    context = "\n\n--\n\n".join([doc.page_content for doc, score in context])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context, question=query)
+    print(prompt)
+    return prompt
+
+if __name__ == "__main__":
+    # Command line input query
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query", type=str, help="The query from user")
+    args = parser.parse_args()
+    query = args.query
+
+    create_prompt(query)
