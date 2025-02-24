2f46dcb80b3ae8f9d6d4f13ee8417617b395e23e
ivankching <ivan1335@gmail.com>
2025-02-24 10:29:29 +0800

    Add post request method to query chat model

--- server/app.py
+++ server/app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from pathlib import Path
+from rag.query_data import create_prompt, chat_model_response
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Index Page"
+
+@app.post('/query')
+def query():
+    query = request.json['query']
+    prompt = create_prompt(query)
+    response = chat_model_response(prompt)
+    return jsonify({"response": response.content})
