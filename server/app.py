7d21bc0f820004237dc327bfa8b96468884f6384
Ivan Ching <ivan1335@gmail.com>
2025-05-14 10:13:41 +0800

    Add CORS middleware

--- server/app.py
+++ server/app.py
@@ -1,12 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from pathlib import Path
 from rag.query_data import create_prompt, chat_model_response
+
+
 
 class Query(BaseModel):
     content: str
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
