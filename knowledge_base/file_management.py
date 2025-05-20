037b86478cb57485e4af94c2540dc1da165d6141
Ivan Ching <ivan1335@gmail.com>
2025-05-20 10:46:28 +0800

    Create new custom type for documents in knowledge base

--- knowledge_base/file_management.py
+++ knowledge_base/file_management.py
@@ -5,8 +5,13 @@ from pathlib import Path
 load_dotenv(find_dotenv())
 ROOT = Path(__file__).parent.parent
 
+class Document:
+    def __init__(self, id: str, title: str):
+        self.id = id
+        self.title = title
+
 def get_files() -> list[Path]:
     data_directory: Path = ROOT / os.environ["DATA_PATH"]
     items: list[Path] = data_directory.glob('**/*')
-    return [file.name for file in items if file.is_file()]
+    return [Document(file.name, file.name) for file in items if file.is_file()]
     
